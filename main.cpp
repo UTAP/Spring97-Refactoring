bdf05b658b4bfcbd70d5c1c92103ce098d0d3c5c
Amir <aseyhani@icloud.com>
2018-03-16 20:49:16 +0330

    all refactorings

--- main.cpp
+++ main.cpp
@@ -1,73 +1,70 @@
 #include<iostream>
 #include<cstdio>
+#include<vector>
 using namespace std;
-int main()
+
+int checkFuse(int m, int c, vector<int> power, vector<bool> situation)
+{
+	int usedpower=0,maxofusedpower=0;
+	int n = power.size();
+	vector<int> toggleSequence;
+	for(int i=0;i<m;i++)
+	{
+		int number;
+		cin >> number;
+		toggleSequence.push_back(number);
+	}
+	for(int i=0;i<m;i++)
+	{
+		int number = toggleSequence[i];
+		situation[number-1]= !situation[number-1];
+		if(situation[number-1])
+			usedpower+=power[number-1];
+		else
+			usedpower-=power[number-1];
+		if(usedpower>maxofusedpower)
+			maxofusedpower=usedpower;
+		if(usedpower>c)
+			return -1;
+	}
+	return maxofusedpower;
+}
+
+void powerSimulator()
 {
 	int n,m,c;
-	int i,j,counter=1;
-	int usedpower=0,maxofusedpower=0;
-	while(1)
+	int counter=1;
+
+	while(true)
 	{
 		cin >> n >> m >> c;
 		if((n==0)&&(m==0)&&(c==0))
-		{
-			return 0;
+			return;
+
+		vector<int> power;
+		for(int i=0;i<n;i++){
+			int p;
+			cin>>p;
+			power.push_back(p);
 		}
-		//int power[20];
-		//int situation[20];
-		int*power=new int [n];
-		int*situation=new int [n];
-		int number;
-		for(i=0;i<n;i++)
+
+		vector<bool> situation;
+		for(int i=0;i<n;i++)
+			situation.push_back(0);
+		int maxofusedpower = checkFuse(m, c, power, situation);
+		if(maxofusedpower == -1)
 		{
-			situation[i]=0;
-		}
-		for(i=0;i<n;i++)
-		{
-			cin >> power[i];
-		}
-		for(i=1;i<(m+1);i++)
-		{
-			cin >> number;
-			for(j=1;j<(n+1);j++)
-			{
-				if((number==j)&&(situation[j-1]==0))
-				{
-					situation[j-1]=1;
-					usedpower=usedpower+power[j-1];
-					if(usedpower>maxofusedpower)
-					{
-						maxofusedpower=usedpower;
-					}
-					break;
-				}
-				if((number==j)&&(situation[j-1]==1))
-				{
-					situation[j-1]=0;
-					usedpower=usedpower-power[j-1];
-					if(usedpower>maxofusedpower)
-					{
-						maxofusedpower=usedpower;
-					}
-					break;
-				}
-			}
-		}
-		if(usedpower>c)
-		{
-			printf("Sequence %d\nFuse was blown.\n\n",counter);
+			cout<<"Sequence "<<counter<<"\nFuse was blown.\n\n";
 			counter++;
-			maxofusedpower=0;
-		    usedpower=0;
-			continue;
 		}
 		else
 		{
-			printf("Sequence %d\nFuse was not blown.\nMaximal power consumption was %d amperes.\n\n",counter,maxofusedpower);
+			cout<<"Sequence "<<counter<<"\nFuse was not blown.\nMaximal power consumption was "<<maxofusedpower<<" amperes.\n\n";
 			counter++;
-			maxofusedpower=0;
-		    usedpower=0;
-			continue;
 		}
 	}
+}
+int main()
+{
+	powerSimulator();
 }
